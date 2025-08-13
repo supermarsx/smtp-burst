f4db0241004f13d816a24110af279b10f1073b14
Mariana <github_618a@imariana.com>
2025-08-13 20:10:41 +0100

    feat: add pipeline action registration

--- smtpburst/pipeline.py
+++ smtpburst/pipeline.py
@@ -16,33 +16,41 @@ from .discovery import nettests, tls_probe, ssl_probe
 logger = logging.getLogger(__name__)
 
 
-ACTION_MAP: Dict[str, Callable[..., Any]] = {
-    "check_dmarc": discovery.check_dmarc,
-    "check_spf": discovery.check_spf,
-    "check_dkim": discovery.check_dkim,
-    "check_srv": discovery.check_srv,
-    "check_soa": discovery.check_soa,
-    "check_txt": discovery.check_txt,
-    "lookup_mx": discovery.lookup_mx,
-    "smtp_extensions": discovery.smtp_extensions,
-    "cert_check": discovery.check_certificate,
-    "port_scan": discovery.port_scan,
-    "probe_honeypot": discovery.probe_honeypot,
-    "tls_discovery": tls_probe.discover,
-    "ssl_discovery": ssl_probe.discover,
-    "open_relay_test": nettests.open_relay_test,
-    "ping": nettests.ping,
-    "traceroute": nettests.traceroute,
-    "vrfy_enum": nettests.vrfy_enum,
-    "expn_enum": nettests.expn_enum,
-    "rcpt_enum": nettests.rcpt_enum,
-    "open_sockets": send.open_sockets,
-    "send": send.bombing_mode,
-    "tcp_syn_flood": attacks.tcp_syn_flood,
-    "tcp_reset_attack": attacks.tcp_reset_attack,
-    "tcp_reset_flood": attacks.tcp_reset_flood,
-    "smurf_test": attacks.smurf_test,
-}
+ACTION_MAP: Dict[str, Callable[..., Any]] = {}
+
+
+def register_action(name: str, func: Callable[..., Any]) -> None:
+    """Register ``func`` under ``name`` for use in pipelines."""
+
+    ACTION_MAP[name] = func
+
+
+# Register built-in actions
+register_action("check_dmarc", discovery.check_dmarc)
+register_action("check_spf", discovery.check_spf)
+register_action("check_dkim", discovery.check_dkim)
+register_action("check_srv", discovery.check_srv)
+register_action("check_soa", discovery.check_soa)
+register_action("check_txt", discovery.check_txt)
+register_action("lookup_mx", discovery.lookup_mx)
+register_action("smtp_extensions", discovery.smtp_extensions)
+register_action("cert_check", discovery.check_certificate)
+register_action("port_scan", discovery.port_scan)
+register_action("probe_honeypot", discovery.probe_honeypot)
+register_action("tls_discovery", tls_probe.discover)
+register_action("ssl_discovery", ssl_probe.discover)
+register_action("open_relay_test", nettests.open_relay_test)
+register_action("ping", nettests.ping)
+register_action("traceroute", nettests.traceroute)
+register_action("vrfy_enum", nettests.vrfy_enum)
+register_action("expn_enum", nettests.expn_enum)
+register_action("rcpt_enum", nettests.rcpt_enum)
+register_action("open_sockets", send.open_sockets)
+register_action("send", send.bombing_mode)
+register_action("tcp_syn_flood", attacks.tcp_syn_flood)
+register_action("tcp_reset_attack", attacks.tcp_reset_attack)
+register_action("tcp_reset_flood", attacks.tcp_reset_flood)
+register_action("smurf_test", attacks.smurf_test)
 
 
 class PipelineError(Exception):
